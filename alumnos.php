606d11d9ff3358ba12c2de1bea8bb6e30f81a1fc
Vic-mrn <vmma0210@gmail.com>
2024-10-22 23:35:42 -0600

    Nuevo pagina de registro etc

--- alumnos.php
+++ alumnos.php
@@ -6,92 +6,69 @@
         <br />
         <h3>Registro de alumno</h3>
         <form class="row g-3">
-
           <!-- Nombre -->
           <div class="col-4">
-            <label class="form-label"
-              >Nombre(s)</label
-            >
-            <input
-              type="text"
-              class="form-control"
-            />
+            <label class="form-label">Nombre(s)</label>
+            <input type="text" class="form-control" />
           </div>
 
           <div class="col-4">
-            <label class="form-label"
-              >Apellido paterno</label
-            >
-            <input
-              type="text"
-              class="form-control"
-            />
+            <label class="form-label">Apellido paterno</label>
+            <input type="text" class="form-control" />
           </div>
 
           <div class="col-4">
-            <label class="form-label"
-              >Apellido materno</label
-            >
-            <input
-              type="text"
-              class="form-control"
-            />
+            <label class="form-label">Apellido materno</label>
+            <input type="text" class="form-control" />
           </div>
 
           <!--  -->
           <div class="col-md-6">
             <label class="form-label">Contraseña</label>
-            <input
-              class="form-control"
-              placeholder="Contraseña"
-            />
+            <input class="form-control" placeholder="Contraseña" />
           </div>
 
           <div class="col-md-6">
             <label for="inputPassword4" class="form-label"
               >Repetir contraseña</label
             >
-            <input
-              class="form-control"
-              placeholder="Repetir contraseña"
-            />
+            <input class="form-control" placeholder="Repetir contraseña" />
 
+            <!--  -->
+          </div>
+          <div class="col-2">
+            <label for="inputAddress" class="form-label">Edad</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="col-10">
+            <label class="form-label">CURP</label>
+            <input type="text" class="form-control" />
+          </div>
 
-          </div>
-          <div class="col-12">
-            <label for="inputAddress" class="form-label">CURP</label>
-            <input
-              type="text"
-              class="form-control"
-              id="inputAddress"
-              placeholder="Ingresa tu CURP - 18 digitos"
-            />
-          </div>
-          <div class="col-12">
-            <label for="inputAddress2" class="form-label">Dirección 2</label>
-            <input
-              type="text"
-              class="form-control"
-              id="inputAddress2"
-              placeholder="Apartment, studio, or floor"
-            />
-          </div>
+          <!--  -->
           <div class="col-md-4">
-            <label for="inputCity" class="form-label">Ciudad</label>
-            <input type="text" class="form-control" id="inputCity" />
-          </div>
-          <div class="col-md-4">
-            <label for="inputState" class="form-label">Estado</label>
-            <select id="inputState" class="form-select">
-              <option selected>Elige...</option>
-              <option>...</option>
+            <label class="form-label">Nivel educativo</label>
+            <select class="form-select">
+              <option selected>Elige alguna opcion</option>
+              <option>Preescolar</option>
+              <option>Primaria</option>
+              <option>Secundaria</option>
             </select>
           </div>
+
           <div class="col-md-4">
-            <label for="inputZip" class="form-label">Código postal</label>
-            <input type="text" class="form-control" id="inputZip" />
+            <label class="form-label">Nivel educativo</label>
+            <select class="form-select">
+              <option selected>Elige alguna opcion</option>
+              <option>1ro</option>
+              <option>2do</option>
+              <option>3ro</option>
+              <option>4to</option>
+              <option>5to</option>
+              <option>6to</option>
+            </select>
           </div>
-          
+
           <div class="col-12">
             <button type="submit" class="btn btn-primary">Ingresar</button>
           </div>
@@ -102,25 +79,3 @@
 </div>
 
 
-
-<div class="col">
-      <div class="mb-3">
-        <br />
-        <h3>Inicio de sesión</h3>
-        <label class="form-label"> Correo electronico </label>
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Correo electronico"
-        />
-      </div>
-      <div class="mb-3">
-        <label class="form-label"> Contraseña </label>
-        <input
-          type="text"
-          class="form-control"
-          id="formGroupExampleInput2"
-          placeholder="Contraseña"
-        />
-      </div>
-    </div>
--- padres.php
+++ padres.php
@@ -1,83 +1,20 @@
 <?php include('includes/header.php'); ?>
-<div class="container">
+<div class="container p-4">
   <div class="row">
-    <div class="col">
-      <div class="mb-3">
-        <br />
-        <h3>¿No tienes cuenta?, Registrate</h3>
-        <form class="row g-3">
-          <div class="col-12">
-            <label for="inputEmail4" class="form-label"
-              >Correo electronico</label
-            >
-            <input
-              type="email"
-              class="form-control"
-              id="inputEmail4"
-              placeholder="Correo electronico"
-            />
-          </div>
-
-          <div class="col-md-6">
-            <label for="inputPassword4" class="form-label">Contraseña</label>
-            <input
-              type="password"
-              class="form-control"
-              id="inputPassword4"
-              placeholder="Contraseña"
-            />
-          </div>
-          <div class="col-md-6">
-            <label for="inputPassword4" class="form-label"
-              >Repetir contraseña</label
-            >
-            <input
-              type="password"
-              class="form-control"
-              id="inputPassword4"
-              placeholder="Repetir contraseña"
-            />
-          </div>
-          <div class="col-12">
-            <label for="inputAddress" class="form-label">CURP</label>
-            <input
-              type="text"
-              class="form-control"
-              id="inputAddress"
-              placeholder="Ingresa tu CURP - 18 digitos"
-            />
-          </div>
-          <div class="col-12">
-            <label for="inputAddress2" class="form-label">Dirección 2</label>
-            <input
-              type="text"
-              class="form-control"
-              id="inputAddress2"
-              placeholder="Apartment, studio, or floor"
-            />
-          </div>
-          <div class="col-md-4">
-            <label for="inputCity" class="form-label">Ciudad</label>
-            <input type="text" class="form-control" id="inputCity" />
-          </div>
-          <div class="col-md-4">
-            <label for="inputState" class="form-label">Estado</label>
-            <select id="inputState" class="form-select">
-              <option selected>Elige...</option>
-              <option>...</option>
-            </select>
-          </div>
-          <div class="col-md-4">
-            <label for="inputZip" class="form-label">Código postal</label>
-            <input type="text" class="form-control" id="inputZip" />
-          </div>
-          
+    <div class="col-6">
+      <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+        <div class="col-6 px-0">
+          <div class="display-5 fst-italic">¿Tienes cuenta?, inicia sesion aqui</div>
           <div class="col-12">
             <button type="submit" class="btn btn-primary">Ingresar</button>
           </div>
-        </form>
+        </div>
+      </div>
+    </div>
+    <div class="col-6">
+      <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+        xd
       </div>
     </div>
   </div>
 </div>
-
